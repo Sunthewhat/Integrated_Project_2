090a1f576d8183f3af1fd13133b37d1f96932f5a
Sunthewhat <chotiwet.wisi@icloud.com>
2024-06-23 22:52:21 +0700

    misc

---
+++ server/src/controllers/user/updateUserData.ts
@@ -0,0 +1,29 @@
+import { Context } from "hono";
+import { UpdateUserData } from "../../services/database/user/update";
+
+const UpdateUserDataController = async (c: Context) => {
+	try {
+		const { userId, username, nameTitle, lastname, email, expectedMonthly, firstname } = await c.req.json();
+		const result = await UpdateUserData(userId, {
+			username,
+			firstname,
+			lastname,
+			email,
+			nameTitle,
+			expectedMonthly,
+		});
+		return c.json({
+			success: true,
+			message: "User data updated",
+			data: result,
+		});
+	} catch (e) {
+		return c.json({
+			success: false,
+			message: e,
+			data: null,
+		});
+	}
+};
+
+export { UpdateUserDataController };
