abae1f2f5ddc1571f3840a886341d05e7576a5a1
Sunthewhat <chotiwet.wisi@icloud.com>
2024-06-26 15:46:59 +0700

    create /

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,7 +4,13 @@ import { userRouter } from "./user";
 import { tradeRouter } from "./trade";
 
 const mainRouter = new Hono();
-
+mainRouter.get("/", (c) => {
+	return c.json({
+		success: true,
+		message: "Welcome to the API",
+		data: null,
+	});
+});
 mainRouter.route("/auth", authRouter);
 mainRouter.route("/user", userRouter);
 mainRouter.route("/trade", tradeRouter);
