00034e2deca89b89cec79563949925aa5842e91f
Sunthewhat <chotiwet.wisi@icloud.com>
2024-07-01 14:42:31 +0700

    fix constant

--- app/lib/constant.dart
+++ app/lib/constant.dart
@@ -1,4 +1,4 @@
 class EnvironmentConstant {
-  // static String baseUrl = 'https://ctrade.sunthewhat.com';
-  static String baseUrl = 'http://10.0.2.2:8000';
+  static String baseUrl = 'https://ctrade.sunthewhat.com';
+  // static String baseUrl = 'http://10.0.2.2:8000';
 }
